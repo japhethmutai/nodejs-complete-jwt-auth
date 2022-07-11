b53cd9d753301924beb32265e612b7e2b5a83d64
Japheth <japhemchirie@gmail.com>
2022-07-11 20:42:18 +0300

    setting up routes with express router

--- routes/subdir.js
+++ routes/subdir.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, '..', 'views', 'subdir', 'index.html'));
+});
+
+router.get('/test(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'subdir', 'test.html'));
+});
 
 module.exports = router;
