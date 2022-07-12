51e0902693cef784421a9770d63980b18ed4df2d
Japheth <japhemchirie@gmail.com>
2022-07-12 14:09:38 +0300

    jwt authentication backend

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,9 @@
 // Cross Origin Resource Sharing
-const whitelist = [
-    'https://www.google.com', 
-    'http://127.0.0.1:3000', 
-    'http://127.0.0.1:5000'
-];
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Access blocked by CORS'));
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,11 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) return res.status(400).json({ 'message': 'Username and password are required.' });
@@ -15,10 +20,32 @@ const handleLogin = async (req, res) => {
 
     if (match) {
         // create a JWTs
-        res.json({ 'success': `User ${username} is logged in!` });
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        // Saving refresh token with current user
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });
     } else {
         res.sendStatus(401);
     }
 }
+
+// While sending in the frontend request, include the option: { credentials: true } to allow backend to set cookie.
 
 module.exports = { handleLogin };
